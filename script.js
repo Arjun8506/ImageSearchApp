b3071954ef41c45262023b7ec0ec747f66fe8dac
Arjun8506 <140004161+Arjun8506@users.noreply.github.com>
2024-04-24 17:28:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let Acces_Key = "XLTj1FfmOW8UeeBxdBMsxZPlmsA3HtTYCAJTq6QxyiU";
+    // target button from html
+    let searchInput = document.getElementById("searchInput");
+    let searchBtn = document.getElementById("searchBtn");
+    let moreBtn = document.querySelector(".loadMore");
+    let showData = document.querySelector(".showData");
+    // creating a new page for shoiwng more images
+    let page = 1;
+    const getData = async (searchValue, pageNo) => {
+        let fetching = await fetch(`https://api.unsplash.com/search/photos?query=${searchValue}&per_Page=28&page=${pageNo}&client_id=${Acces_Key}`);
+        let jsonData = await fetching.json();
+        console.log(jsonData.results);
+
+        for (let i = 0; i < jsonData.results.length; i++) {
+            const newImg = document.createElement("img");
+            newImg.src = jsonData.results[i].urls.full;
+            showData.appendChild(newImg);
+
+            newImg.addEventListener("click", () => {
+                document.querySelector(".showingImage").style.display = "block"
+                document.getElementById("showingImageInBig").src = jsonData.results[i].urls.full
+                document.getElementById("close").addEventListener("click", () => {
+                    document.querySelector(".showingImage").style.display = "none"
+                })
+            })
+        }
+        moreBtn.style.display = "block"
+    }
+    searchBtn.addEventListener("click", function () {
+        let searchValue = searchInput.value;
+        getData(searchValue, 1);
+    });
+    moreBtn.addEventListener("click", function () {
+        let searchValue = searchInput.value;
+        getData(searchValue, page++);
+    });
+});
